e2c901af1d3fc1bad2b78487a9db21d5b88cc257
Steven <stevenanthonyzanga@gmail.com>
2019-11-28 20:16:55 -0500

    added pdf generation

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,176 +1,189 @@
 <template>
     <div class="container">
-        <table class="table">
-            <thead>
-                <tr>
-                <th scope="col">Class</th>
-                <th scope="col">Hours</th>
-                <th scope="col">Date</th>
-                <th scope="col">Module #</th>
-                <th scope="col">Module Name</th>
-                <th scope="col">Teacher</th>
-                <th scope="col">Year</th>
-                
-                
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <th scope="row">1</th>
-                    <td>6</td>
-                    <td>{{dates[0]}}</td>
-                    <td>946452</td>
-                    <td>Business Proposal & Training process</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
-
-                <tr>
-                <th scope="row">2</th>
-                    <td>6</td>
-                    <td>{{dates[1]}}</td>
-                    <td>946452</td>
-                    <td>Business Proposal & Training process</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
+        <div id="schedule">
+            <div class="col text-center">
+                <h2><img id="logo" src="../assets/emsb.png" alt=""> Schedule for {{teacher}} <img id="logo" src="../assets/emsb.png" alt=""></h2>
+            </div>
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Class</th>
+                    <th scope="col">Hours</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Module #</th>
+                    <th scope="col">Module Name</th>
+                    <th scope="col">Teacher</th>
+                    <th scope="col">Year</th>
                     
-                </tr>
+                    
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                        <td>6</td>
+                        <td>{{dates[0]}}</td>
+                        <td>946452</td>
+                        <td>Business Proposal & Training process</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
 
-                <tr>
-                <th scope="row">3</th>
-                    <td>6</td>
-                    <td>{{dates[2]}}</td>
-                    <td>946452</td>
-                    <td>Business Proposal & Training process</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                        <td>6</td>
+                        <td>{{dates[1]}}</td>
+                        <td>946452</td>
+                        <td>Business Proposal & Training process</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                        
+                    </tr>
 
-                <tr>
-                <th scope="row">4</th>
-                    <td>6</td>
-                    <td>{{dates[3]}}</td>
-                    <td>946462 / 946472</td>
-                    <td>Computer Tools / Business Plan Outline</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                        <td>6</td>
+                        <td>{{dates[2]}}</td>
+                        <td>946452</td>
+                        <td>Business Proposal & Training process</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
 
-                <tr>
-                <th scope="row">5</th>
-                    <td>6</td>
-                    <td>{{dates[4]}}</td>
-                    <td>946462 / 946472</td>
-                    <td>Computer Tools / Business Plan Outline</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
+                    <tr>
+                    <th scope="row">4</th>
+                        <td>6</td>
+                        <td>{{dates[3]}}</td>
+                        <td>946462 / 946472</td>
+                        <td>Computer Tools / Business Plan Outline</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
 
-                <tr>
-                    <th scope="row">6</th>
-                    <td>6</td>
-                    <td>{{dates[5]}}</td>
-                    <td>946462 / 946472</td>
-                    <td>Computer Tools / Business Plan Outline</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
+                    <tr>
+                    <th scope="row">5</th>
+                        <td>6</td>
+                        <td>{{dates[4]}}</td>
+                        <td>946462 / 946472</td>
+                        <td>Computer Tools / Business Plan Outline</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
 
-                <tr>
-                    <th scope="row">7</th>
-                    <td>6</td>
-                    <td>{{dates[6]}}</td>
-                    <td>946487</td>
-                    <td>Marketing And Sales</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
+                    <tr>
+                        <th scope="row">6</th>
+                        <td>6</td>
+                        <td>{{dates[5]}}</td>
+                        <td>946462 / 946472</td>
+                        <td>Computer Tools / Business Plan Outline</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
 
-                <tr>
-                    <th scope="row">8</th>
-                    <td>6</td>
-                    <td>{{dates[7]}}</td>
-                    <td>946487</td>
-                    <td>Marketing And Sales</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
+                    <tr>
+                        <th scope="row">7</th>
+                        <td>6</td>
+                        <td>{{dates[6]}}</td>
+                        <td>946487</td>
+                        <td>Marketing And Sales</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
 
-                <tr>
-                    <th scope="row">9</th>
-                    <td>6</td>
-                    <td>{{dates[8]}}</td>
-                    <td>946487</td>
-                    <td>Marketing And Sales</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
+                    <tr>
+                        <th scope="row">8</th>
+                        <td>6</td>
+                        <td>{{dates[7]}}</td>
+                        <td>946487</td>
+                        <td>Marketing And Sales</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
 
-                <tr>
-                    <th scope="row">10</th>
-                    <td>6</td>
-                    <td>{{dates[9]}}</td>
-                    <td>946487</td>
-                    <td>Marketing And Sales</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
+                    <tr>
+                        <th scope="row">9</th>
+                        <td>6</td>
+                        <td>{{dates[8]}}</td>
+                        <td>946487</td>
+                        <td>Marketing And Sales</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
 
-                <tr>
-                    <th scope="row">11</th>
-                    <td>6</td>
-                    <td>{{dates[10]}}</td>
-                    <td>946494</td>
-                    <td>Planning Resources</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
+                    <tr>
+                        <th scope="row">10</th>
+                        <td>6</td>
+                        <td>{{dates[9]}}</td>
+                        <td>946487</td>
+                        <td>Marketing And Sales</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
 
-                <tr>
-                    <th scope="row">12</th>
-                    <td>6</td>
-                    <td>{{dates[11]}}</td>
-                    <td>946494</td>
-                    <td>Planning Resources</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
+                    <tr>
+                        <th scope="row">11</th>
+                        <td>6</td>
+                        <td>{{dates[10]}}</td>
+                        <td>946494</td>
+                        <td>Planning Resources</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
 
-                <tr>
-                    <th scope="row">13</th>
-                    <td>6</td>
-                    <td>{{dates[12]}}</td>
-                    <td>946505</td>
-                    <td>Financial Plan</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
+                    <tr>
+                        <th scope="row">12</th>
+                        <td>6</td>
+                        <td>{{dates[11]}}</td>
+                        <td>946494</td>
+                        <td>Planning Resources</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
 
-                <tr>
-                    <th scope="row">14</th>
-                    <td>6</td>
-                    <td>{{dates[13]}}</td>
-                    <td>946505</td>
-                    <td>Financial Plan</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
+                    <tr>
+                        <th scope="row">13</th>
+                        <td>6</td>
+                        <td>{{dates[12]}}</td>
+                        <td>946505</td>
+                        <td>Financial Plan</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
 
-                <tr>
-                    <th scope="row">15</th>
-                    <td>6</td>
-                    <td>{{dates[14]}}</td>
-                    <td>946505</td>
-                    <td>Business Plan</td>
-                    <td>{{teacher}}</td>
-                    <td>2020/2021</td>
-                </tr>
-            </tbody>
-        </table>
+                    <tr>
+                        <th scope="row">14</th>
+                        <td>6</td>
+                        <td>{{dates[13]}}</td>
+                        <td>946505</td>
+                        <td>Financial Plan</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
+
+                    <tr>
+                        <th scope="row">15</th>
+                        <td>6</td>
+                        <td>{{dates[14]}}</td>
+                        <td>946505</td>
+                        <td>Business Plan</td>
+                        <td>{{teacher}}</td>
+                        <td>2020/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col text-center">
+            <form @submit.prevent="generatePDF">
+                <input type="submit" value="Generate PDF" class="btn-info col-md-6">    
+            </form>
+        </div>
+        <br>
     </div>
 </template>
 
 <script>
+import html2pdf from 'html2pdf.js';
+
 export default {
     data() {
         return {
@@ -189,57 +202,66 @@ export default {
         }
     },
     methods: {
-        calculateDates(start, end){
-            // eslint-disable-next-line no-console
-            console.log(start, end);
+        generatePDF(){
+            var element = document.getElementById("schedule");
+            var opt = {
+                margin:       0.4,
+                filename:     'schedule.pdf',    
+                html2canvas:  { scale: 2 },
+                jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+            };
+            html2pdf().set(opt).from(element).save();
         }
     },
 
     created(){
-        var i
-        for(i = 0; i < 15; i++){
-            
-            var starting = this.$props.start.split('/');
-            starting[1] = starting[1] - 1; 
-            var s = new Date(starting[2], starting[1], starting[0]);
+    
+        var starting = this.$props.start.split('/');
+        starting[1] = starting[1] - 1; 
+        var s = new Date(starting[2], starting[1], starting[0]);
 
-            var ending = this.$props.end.split('/');
-            ending[1] = ending[1] - 1;
-            var e = new Date(ending[2], ending[1], ending[0]);
+        var ending = this.$props.end.split('/');
+        ending[1] = ending[1] - 1;
+        var e = new Date(ending[2], ending[1], ending[0]);
 
+        var i;
+        for(i = s; i <= e; i.setDate(i.getDate() + 7)){
+            // I need to check if the date is part of
+            // some dates that are non teaching days.
 
-            for(i = s; i <= e; i.setDate(i.getDate() + 7)){
-                // I need to check if the date is part of
-                // some dates that are non teaching days.
-
-                // Christmas
-                var christmasStart = new Date(2019, 11, 23);
-                var christmasEnd = new Date(2020, 0, 3);
-                if(i >= christmasStart && i <= christmasEnd) {
-                    // eslint-disable-next-line no-console
-                    console.log("Skipping: " + i)
-                    continue;
-                }
-
-                // Summer Break
-
-                
-
-
-                this.dates.push(i.getDate() + "/" + (i.getMonth()+1) + "/" + i.getFullYear());
-                //console.log(this.dates);
+            // Christmas
+            var christmasStart = new Date(2019, 11, 23);
+            var christmasEnd = new Date(2020, 0, 3);
+            if(i >= christmasStart && i <= christmasEnd) {
+                // eslint-disable-next-line no-console
+                console.log("Skipping: " + i)
+                continue;
             }
 
+            // Summer Break
+
             
 
 
-
+            this.dates.push(i.getDate() + "/" + (i.getMonth()+1) + "/" + i.getFullYear());
+            //console.log(this.dates);
         }
+
+        
+
+
+
+    
     }
     
 }
 </script>
 
 <style>
+    #logo {
+        width: 10%;
+        height: 10%;
+    }
+
 
 </style>
